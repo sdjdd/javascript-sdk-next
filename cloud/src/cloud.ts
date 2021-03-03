159f17ee04f686503c25ab0e0618402536382725
sdjdd <i@sdjdd.com>
2021-03-03 18:17:18 +0800

    feat: push notification

--- cloud/src/cloud.ts
+++ cloud/src/cloud.ts
@@ -1,5 +1,20 @@
-import type { App, AuthOptions } from '../../core';
+import type { App, AuthOptions, Query } from '../../core';
 import { Captcha, RequestCaptchaOptions } from './captcha';
+
+export interface PushOptions extends AuthOptions {
+  query?: Query<any>;
+  channels?: string[];
+  pushTime?: Date;
+  expirationTime?: Date;
+  expirationInterval?: number;
+  notificationId?: string;
+  reqId?: string;
+  prod?: 'prod' | 'dev';
+  topic?: string;
+  apnsTeamId?: string;
+  flowControl?: number;
+  notificationChannel?: string;
+}
 
 export class Cloud {
   constructor(public readonly app: App) {}
@@ -41,5 +56,34 @@ export class Cloud {
       options
     );
     return db.decode(result);
+  }
+
+  async sendPushNotification(data: Record<string, any>, options?: PushOptions): Promise<void> {
+    if (options?.expirationTime && options?.expirationInterval) {
+      throw new Error('Cannot set both expirationTime and expirationInterval');
+    }
+    await this.app.request(
+      {
+        service: 'push',
+        method: 'POST',
+        path: '/1.1/push',
+        body: {
+          data,
+          where: options?.query?.params.where,
+          channels: options?.channels,
+          push_time: options?.pushTime?.toISOString(),
+          expiration_time: options?.expirationTime?.toISOString(),
+          expiration_interval: options?.expirationInterval,
+          notification_id: options?.notificationId,
+          req_id: options?.reqId,
+          prod: options?.prod,
+          topic: options?.topic,
+          apns_team_id: options?.apnsTeamId,
+          flow_control: options?.flowControl,
+          _notificationChannel: options?.notificationChannel,
+        },
+      },
+      options
+    );
   }
 }
