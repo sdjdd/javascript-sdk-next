f1c3f111e38bb8dfcfbd76fc59112c6cd95e8ff1
sdjdd <i@sdjdd.com>
2021-06-22 15:37:30 +0800

    feat(core): adjust browser field

---
+++ core/browser/index.js
@@ -0,0 +1,4 @@
+const adapters = require('@leancloud/platform-adapters-browser');
+const LC = require('../dist/index.js');
+LC.setAdapters(adapters);
+module.exports = LC;
--- core/browser/index.mjs
+++ core/browser/index.mjs
@@ -1,4 +1,4 @@
 import * as adapters from '@leancloud/platform-adapters-browser';
-import { setAdapters } from './dist/index.mjs';
+import { setAdapters } from '../dist/index.mjs';
 setAdapters(adapters);
-export * from './dist/index.mjs';
+export * from '../dist/index.mjs';
